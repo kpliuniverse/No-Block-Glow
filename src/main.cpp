bd8f3ed1fb013c03ea62e596f437767051009e19
kazido <92008061+kazido@users.noreply.github.com>
2024-08-29 23:06:27 -0500

    added option for to disable no glow

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ class $modify(LevelEditorLayer) {
 	GameObject* createObject(int p0, cocos2d::CCPoint p1, bool p2) {
 		GameObject* object = LevelEditorLayer::createObject(p0, p1, p2);
 
-		object->m_hasNoGlow = true;
+		object->m_hasNoGlow = Mod::get()->getSettingValue<bool>("no-glow");
 		object->m_isDontFade = Mod::get()->getSettingValue<bool>("dont-fade");
 		object->m_isDontEnter = Mod::get()->getSettingValue<bool>("dont-enter");
 
